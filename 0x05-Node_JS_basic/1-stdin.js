ba03b682b1a8caf7e9ef708d6bb15dcb02ea4c54
Prisca Chidimma Maduka <88077456+Priceless-P@users.noreply.github.com>
2024-06-04 22:17:34 +0100

    Task done

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,13 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-process.stdin.on('data', () => {
-  const input = process.stdin.read();
-    if (data) {
-        process.stdout.write(`Your name is: ${input}\n`);
-    }
+
+process.stdin.on('readable', () => {
+  const data = process.stdin.read();
+
+  if (data) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
 });
+
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
