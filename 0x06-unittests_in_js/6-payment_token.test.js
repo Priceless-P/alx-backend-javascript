cc03acc39e86b87aa05b15a6f40899861a401d2c
Prisca Chidimma Maduka <88077456+Priceless-P@users.noreply.github.com>
2024-06-06 21:57:19 +0100

    Correction

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -2,8 +2,8 @@ const getPaymentTokenFromAPI = require('./6-payment_token')
 const chai = require('chai')
 const expect = chai.expect;
 
-describe('getPaymentTokenFromAPI', (done) => {
-    it('should return a successful response from the API when success is true', () => {
+describe('getPaymentTokenFromAPI', () => {
+    it('should return a successful response from the API when success is true', (done) => {
         getPaymentTokenFromAPI(true)
             .then((response) => {
                 expect(response).to.deep.equal({ data: 'Successful response from the API' });
