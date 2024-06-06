c8fcc9725e18f416710b2b6d8fff044cd2a0600e
Prisca Chidimma Maduka <88077456+Priceless-P@users.noreply.github.com>
2024-06-06 21:55:48 +0100

    Correct

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -1,7 +1,8 @@
-const getPaymentTokenFromAPI = (success) => {
-    if (success) {
-      return Promise.resolve({ data: 'Successful response from the API' });
-    }
-  }
-
-  module.exports = getPaymentTokenFromAPI;
+const getPaymentTokenFromAPI = success => {
+    return new Promise((resolve, _reject) => {
+        if (success === true){
+            resolve({ data: 'Successful response from the API' });
+        }
+    });
+}
+module.exports = getPaymentTokenFromAPI;
